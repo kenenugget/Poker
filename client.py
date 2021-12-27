9c373f50cb0e5ba0517f3c14e72c59ac545c0a3d
kenenugget <nugget.dk56@gmail.com>
2021-12-27 22:51:54 +1300

    Fixed server bug

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 client.sendall(bytes("This is from Client",'UTF-8'))
+
 while True:
   in_data =  client.recv(1024)
   print("From Server :" ,in_data.decode())
--- server.py
+++ server.py
@@ -7,20 +7,25 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((LOCALHOST, PORT))
 server.listen(1)
 
-server.bind((LOCALHOST, PORT))
-server.listen(1)
 print("Server started")
 print("Waiting for client request..")
+
 clientConnection,clientAddress = server.accept()
+
 print("Connected clinet :" , clientAddress)
+
 msg = ''
+
 while True:
   in_data = clientConnection.recv(1024)
   msg = in_data.decode()
+
   if msg=='bye':
     break
+
   print("From Client :" , msg)
   out_data = input()
   clientConnection.send(bytes(out_data,'UTF-8'))
+
 print("Client disconnected....")
 clientConnection.close()
