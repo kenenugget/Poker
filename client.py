c8575d7c93ea9f4a17981beeeba5143c2a41d35b
kenenugget <nugget.dk56@gmail.com>
2021-12-28 11:39:03 +1300

    Input

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ client.sendall(bytes("This is from Client",'UTF-8'))
 while True:
   in_data =  client.recv(1024)
   print("From Server :" ,in_data.decode())
-  out_data = input()
+  out_data = input("Message: ")
   client.sendall(bytes(out_data,'UTF-8'))
   if out_data=='bye':
     break
