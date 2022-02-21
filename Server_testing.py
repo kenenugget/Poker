b819c2a88963e1e59481a4f913a15eb2b38d551d
kenenugget <nugget.dk56@gmail.com>
2022-02-21 21:59:37 +1300

    Server reply (try 1)

--- Server_testing.py
+++ Server_testing.py
@@ -19,11 +19,12 @@ def multi_threaded_client(connection):
     connection.send(str.encode('Server is working:'))
     while True:
         data = connection.recv(2048)
-        response = 'Server message: ' + data.decode('utf-8')
+
+        response = input("Response: ")
         
         if not data:
             break
-        connection.sendall(str.encode(response))
+        connection.sendall(str.encode("server message: " + response))
     connection.close()
 
 while True:
